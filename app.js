11483ee2a17296ffd872ae1a60c5ae47d168ac5e
Timothee Groleau <timothee.groleau@gmail.com>
2019-10-06 21:43:03 +0800

    Relay twitch chat to UI via web socket

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 'use strict'
 
-const path = require('path')
-const AutoLoad = require('fastify-autoload')
+const path = require('path');
+const AutoLoad = require('fastify-autoload');
+const config = require('./config');
 
 module.exports = function (fastify, opts, next) {
   // Place here your custom code!
@@ -30,3 +31,62 @@ module.exports = function (fastify, opts, next) {
   // Make sure to call next when done
   next();
 }
+
+
+// set up twitch bot
+const WebSocket = require('ws');
+ 
+const wss = new WebSocket.Server({ port: 3339 });
+
+const connections = new Set();
+
+wss.on('connection', function connection(ws) {
+
+  console.log('connection on 3339');
+
+  connections.add(ws);
+
+  ws.on('close', () => {
+    connections.delete(ws);
+  });
+
+  ws.on('message', console.log);
+});
+
+
+const ConnectionAPI = new Proxy({}, {
+  get: function(target, prop, ...args) {
+    return function(...args) {
+      const msg = JSON.stringify([prop, ...args]);
+
+      [...connections].forEach(ws => {
+        ws.send(msg);
+      });
+    }
+  }
+});
+
+
+const TwitchBot = require('twitch-bot')
+
+const Bot = new TwitchBot({
+  username: 'yobi9',
+  oauth: config.twitch.oauth,
+  channels: ['yobi9']
+});
+
+Bot.on('join', channel => {
+  console.log(`Joined channel: ${channel}`)
+});
+
+['connected', 'error', 'close', 'timeout', 'ban', 'message', 'part', 'subscription'].forEach(evt => {
+  Bot.on(evt, data => {
+    console.log(evt, data);
+  });
+});
+
+Bot.on('message', chatter => {
+  ConnectionAPI.message(chatter);
+});
+
+
