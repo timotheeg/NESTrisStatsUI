cc5d93f31db1f83b1b8ef927920e76b8eadfa0ce
Timothee Groleau <tgroleau@zendesk.com>
2019-11-15 00:02:32 +0800

    Update config exampe to document voiceover setup

--- config.example.js
+++ config.example.js
@@ -1,8 +1,23 @@
 module.exports = {
-  twitch: {
-    username: 'USERNAME_GOES_HERE',
-    oauth:    'KEY_GOES_HERE',
-    channels: ['CHANNEL_GOES_HERE']
-  },
-  default_player: 'PLAYER_NAME_GOES_HERE'
-}
+	twitch: {
+		username: 'USERNAME_GOES_HERE',
+		oauth:    'KEY_GOES_HERE',
+		channels: ['CHANNEL_GOES_HERE']
+	},
+	default_player: 'PLAYER_NAME_GOES_HERE',
+	chat_voice: {
+		enabled: true,
+		provider: 'google',
+		credentials: './GCP_TwitchChatVoiceOver.json',
+		twitch_mappings: {
+			osx: {
+				puffy2303: 'Karen',
+				yobi9:     'Daniel'
+			},
+			google: {
+				puffy2303: 'en-AU-Wavenet-A',
+				yobi9:     'en-GB-Wavenet-B'
+			}
+		}
+	}
+};
