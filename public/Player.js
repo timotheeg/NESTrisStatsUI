ace8f2c00d4871d6d4a10dec87ee9fa74638a199
Timothee Groleau <tgroleau@zendesk.com>
2020-04-03 07:53:50 +0800

    Competition: Start moving rendering functions to Player class

--- public/Player.js
+++ public/Player.js
@@ -1,7 +1,127 @@
+if (!CanvasRenderingContext2D.prototype.clear) {
+	CanvasRenderingContext2D.prototype.clear = function (preserveTransform) {
+		if (preserveTransform) {
+			this.save();
+			this.setTransform(1, 0, 0, 1, 0, 0);
+		}
+
+		this.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+		if (preserveTransform) {
+			this.restore();
+		}
+	};
+}
+
+
+function renderBlock(level, block_index, pixel_size, ctx, pos_x, pos_y) {
+	let color;
+
+	switch (block_index) {
+		case 1:
+			// inefficient because it draws the area twice
+			// check speed and optimize if necessary
+			color = LEVEL_COLORS[level % 10][0];
+
+			ctx.fillStyle = color;
+			ctx.fillRect(
+				pos_x,
+				pos_y,
+				pixel_size * 7,
+				pixel_size * 7
+			);
+
+			ctx.fillStyle = 'white';
+			ctx.fillRect(
+				pos_x,
+				pos_y,
+				pixel_size,
+				pixel_size
+			);
+
+			ctx.fillRect(
+				pos_x + pixel_size,
+				pos_y + pixel_size,
+				pixel_size * 5,
+				pixel_size * 5
+			);
+
+			break;
+
+		case 2:
+		case 3:
+			color = LEVEL_COLORS[level % 10][block_index - 2];
+
+			ctx.fillStyle = color;
+			ctx.fillRect(
+				pos_x,
+				pos_y,
+				pixel_size * 7,
+				pixel_size * 7
+			);
+
+			ctx.fillStyle = 'white';
+			ctx.fillRect(
+				pos_x,
+				pos_y,
+				pixel_size,
+				pixel_size
+			);
+			ctx.fillRect(
+				pos_x + pixel_size,
+				pos_y + pixel_size,
+				pixel_size * 2,
+				pixel_size
+			);
+			ctx.fillRect(
+				pos_x + pixel_size,
+				pos_y + pixel_size * 2,
+				pixel_size,
+				pixel_size
+			);
+
+			break;
+
+		default:
+			/*
+			ctx.clearRect(
+				pos_x,
+				pos_y,
+				pixel_size * 7,
+				pixel_size * 7
+			);
+			/**/
+	}
+}
+
+
+
+/*
+  dom: {
+  	score:   text element
+	level:   text element
+	lines:   text element
+	preview: canva element
+	field:   canva element
+  }
+
+  options: {
+	preview_pixel_size: int
+	field_pixel_size: int
+  }
+ */
+
 class Player {
-	constructor(name, dom) {
+	constructor(name, dom, options) {
 		this.name = name;
 		this.dom = dom;
+		this.options = options;
+
+		this.field_pixel_size = this.options.field_pixel_size || this.options.pixel_size;
+		this.preview_pixel_size = this.options.preview_pixel_size || this.options.pixel_size;
+
+		this.preview_ctx = this.dom.preview.getContext('2d');
+		this.field_ctx = this.dom.field.getContext('2d');
 
 		this.reset();
 	}
@@ -12,29 +132,19 @@ class Player {
 		this.lines = 0;
 		this.level = 0;
 		this.rtr = 0;
+
+		this.preview_ctx.clear();
+		this.field_ctx.clear();
 	}
 
 	getScore() {
 		return this.score;
 	}
 
-	setScoreDiff(difference) {
-		dom.score_diff.textContent = difference;
-
-		if (difference < 0) {
-			dom.score_diff.classList.remove('winning');
-			dom.score_diff.classList.add('losing');
-		}
-		else {
-			dom.score_diff.classList.remove('losing');
-			dom.score_diff.classList.add('winning');
-		}
-	}
-
 	onFrame(data) {
-		dom.score.textContent = data.score;
-		dom.lines.textContent = data.lines;
-		dom.level.textContent = data.level;
+		this.dom.score.textContent = data.score;
+		this.dom.lines.textContent = data.lines;
+		this.dom.level.textContent = data.level;
 
 		this.score = parseInt(data.score, 10);
 		this.lines = parseInt(data.lines, 10);
@@ -44,11 +154,116 @@ class Player {
 		this.renderPreview(this.level, data.preview);
 	}
 
-	renderPreview() {
+	renderPreview(level, preview) {
+		const piece_id = `${level}${preview}`;
 
+		if (piece_id === this.current_preview) return;
+
+		this.current_preview = piece_id;
+
+		const
+			ctx              = this.preview_ctx,
+			col_index        = PIECE_COLORS[next_piece],
+			pixels_per_block = this.preview_pixel_size * (7 + 1);
+			x_offset_3       = Math.floor((ctx.canvas.width - pixels_per_block * 3 + BLOCK_PIXEL_SIZE) / 2),
+			positions        = [];
+
+		ctx.clear();
+
+		let
+			pos_x = 0,
+			pos_y = 0,
+			x_idx = 0;
+
+		switch(next_piece) {
+			case 'I':
+				pos_y = Math.floor((ctx.canvas.height - BLOCK_PIXEL_SIZE * 7) / 2);
+
+				positions.push([x_idx++ * pixels_per_block, pos_y]);
+				positions.push([x_idx++ * pixels_per_block, pos_y]);
+				positions.push([x_idx++ * pixels_per_block, pos_y]);
+				positions.push([x_idx++ * pixels_per_block, pos_y]);
+				break;
+
+			case 'O':
+				pos_x = Math.floor((ctx.canvas.width - pixels_per_block * 2 + BLOCK_PIXEL_SIZE) / 2);
+
+				positions.push([pos_x, pos_y]);
+				positions.push([pos_x, pos_y + pixels_per_block]);
+				positions.push([pos_x + pixels_per_block, pos_y]);
+				positions.push([pos_x + pixels_per_block, pos_y + pixels_per_block]);
+				break;
+
+			case 'T':
+			case 'J':
+			case 'L':
+				// top line is the same for both pieces
+				positions.push([x_offset_3 + x_idx++ * pixels_per_block, pos_y]);
+				positions.push([x_offset_3 + x_idx++ * pixels_per_block, pos_y]);
+				positions.push([x_offset_3 + x_idx++ * pixels_per_block, pos_y]);
+
+				if (next_piece == 'L') {
+					x_idx = 0;
+				}
+				else if (next_piece == 'T') {
+					x_idx = 1;
+				}
+				else {
+					x_idx = 2;
+				}
+
+				positions.push([x_offset_3 + x_idx * pixels_per_block, pos_y + pixels_per_block]);
+				break;
+
+			case 'Z':
+			case 'S':
+				positions.push([x_offset_3 + pixels_per_block, pos_y]);
+				positions.push([x_offset_3 + pixels_per_block, pos_y + pixels_per_block]);
+
+				if (next_piece == 'Z') {
+					positions.push([x_offset_3, pos_y]);
+					positions.push([x_offset_3 + pixels_per_block * 2, pos_y + pixels_per_block]);
+				}
+				else {
+					positions.push([x_offset_3, pos_y + pixels_per_block]);
+					positions.push([x_offset_3 + pixels_per_block * 2, pos_y]);
+				}
+		}
+
+		positions.forEach(([pos_x, pos_y]) => {
+			renderBlock(
+				level,
+				col_index,
+				this.preview_pixel_size,
+				ctx,
+				pos_x,
+				pos_y
+			);
+		});
 	}
 
-	renderField() {
+	renderField(level, field_string) {
+		const stage_id = `${level}${field_string}`;
 
+		if (stage_id === this.current_field) return;
+
+		this.current_field = stage_id;
+
+		const pixels_per_block = this.field_pixel_size * (7 + 1);
+
+		this.field_ctx.clear();
+
+		for (let x = 0; x < 10; x++) {
+			for (let y = 0; y < 20; y++) {
+				renderBlock(
+					level,
+					parseInt(stage_string[y * 10 + x], 10),
+					this.field_pixel_size,
+					this.field_ctx,
+					x * pixels_per_block,
+					y * pixels_per_block
+				);
+			}
+		}
 	}
 }
