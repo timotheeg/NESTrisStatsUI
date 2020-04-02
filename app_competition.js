5e5e2493bbfa64135cdb6c1b33d5bd5592c6c1e3
Timothee Groleau <tgroleau@zendesk.com>
2020-04-02 23:43:10 +0800

    app_competition getting ready to consume OCR streams

--- app_competition.js
+++ app_competition.js
@@ -1,30 +1,72 @@
 const WebSocket = require('ws');
- 
-const wss = new WebSocket.Server({ port: 3338 });
+const NEStrisServer = require('./NesTrisServer');
 
-const connections = new Set();
+const admin_port   = 4000;
+const player1_port = 4001;
+const player2_port = 4002;
+const view_port    = 4003;
 
-wss.on('connection', function connection(ws) {
 
-  connections.add(ws);
+console.log('Setting up Viewer WS server');
 
-  ws.on('close', () => {
-    connections.delete(ws);
-  });
+const viewer_wss = new WebSocket.Server({ port: view_port });
 
-  ws.on('message', console.log);
+const viewer_conns = new Set();
 
+viewer_wss.on('connection', function connection(ws) {
+	viewer_conns.add(ws);
+
+	ws.on('close', () => {
+		viewer_conns.delete(ws);
+	});
+});
+
+function RPCToViewers(...args) {
+	const msg = JSON.stringify(args);
+
+	[...viewer_conns].forEach(ws => {
+		ws.send(msg);
+	});
+}
+
+
+console.log('Setting up Admin WS server');
+
+const admin_wss = new WebSocket.Server({ port: admin_port });
+
+let admin_conn;
+
+admin_wss.on('connection', function connection(ws) {
+	if (admin_conn) {
+		admin_conn.removeAllListeners('message');
+	}
+
+	admin_conn = ws;
+
+	admin_conn.on('message', message => {
+		RPCToViewers(...JSON.parse(message));
+	});
 });
 
 
-module.exports = new Proxy({}, {
-  get: function(target, prop, ...args) {
-    return function(...args) {
-      const msg = JSON.stringify([prop, ...args]);
+console.log('Setting up Admin CLI WS server');
 
-      [...connections].forEach(ws => {
-        ws.send(msg);
-      });
-    }
-  }
+const ViewAPI = new Proxy({}, {
+	get: function(target, prop, ...args) {
+		return function(...args) {
+			RPCToViewers(prop, ...args);
+		}
+	}
 });
+
+
+console.log('Setting up NesTris server for player 1');
+
+const server_p1 = new NEStrisServer(player1_port, 'player 1');
+const server_p2 = new NEStrisServer(player2_port, 'player 2');
+
+server_p1.on('frame', data => RPCToViewers(['frame', 1, data]));
+server_p2.on('frame', data => RPCToViewers(['frame', 2, data]));
+
+
+module.exports = ViewAPI;
