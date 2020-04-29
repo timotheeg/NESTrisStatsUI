53441a6d3148d3a32c411c098bbc3630e84ec987
Timothee Groleau <tgroleau@zendesk.com>
2020-04-29 17:52:29 +0800

    Progammatic piece background for CTJC layout

--- public/utils.js
+++ public/utils.js
@@ -26,3 +26,12 @@ function css_size(css_pixel_width) {
 function peek(arr) {
 	return arr[arr.length - 1];
 }
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    return array;
+}
