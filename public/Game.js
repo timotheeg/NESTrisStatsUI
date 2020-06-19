6884cd917f70f948b221456e1c97a52920b2156e
Timothee Groleau <tgroleau@zendesk.com>
2020-06-19 18:34:50 +0800

    Add board stats analysis

--- public/Game.js
+++ public/Game.js
@@ -1,3 +1,125 @@
+const FIELD_ANALYSIS_IGNORED_ROWS = 3;
+
+class Row {
+	constructor(line, row_idx) {
+		this.cells = line.split('');
+		this.idx = row_idx;
+
+		const emptys = this.emptys = [];
+
+		this.cells.forEach((val, idx) => {
+			if (val === '0') emptys.push(idx);
+		});
+
+		if (emptys.length === 1) {
+			this.well = emptys[0] + 1;
+		}
+	}
+}
+
+class Column {
+	constructor(cells, col_idx) {
+		this.cells = cells;
+		this.idx = col_idx;
+
+		const len = this.cells.length;
+
+		this.top_fill_idx = len;
+
+		for (let idx = 0; idx < len; idx++) {
+			if (this.cells[idx] != '0') {
+				this.top_fill_idx = idx;
+				break;
+			}
+		}
+
+		this.has_holes = this.cells.indexOf('0', this.top_fill_idx) > -1;
+	}
+}
+
+class Board {
+	constructor(board_str, top_rows_to_ignore) {
+		this.rows = Array();
+
+		const columns = Array(10).fill().map(_ => []);
+
+		for (let y = top_rows_to_ignore; y < 20; y++) {
+			const row = new Row(board_str.substr(10 * y, 10), this.rows.length);
+			this.rows.push(row);
+
+			row.cells.forEach((value, idx) => {
+				columns[idx].push(value);
+			});
+		}
+
+		this.columns = columns.map((col, idx) => new Column(col, idx));
+
+		const tetris_top_row = this.getTetrisTopRow();
+
+		this.stats = {
+			top_idx:           columns.reduce((acc, col) => Math.min(acc, col.top_fill_idx), 20),
+			is_tetris_ready:   !!this.tetris_top_row,
+			has_perfect_slope: this.hasPerfectSlope(),
+			has_double_well:   this.hasDoubleWell(),
+		};
+	}
+
+	getTetrisTopRow() {
+		for (let idx = this.rows.length; idx-- > 4;) {
+			const row = this.rows[idx];
+
+			if (!row.well) continue;
+
+			// blocked below and free above
+			if (this.columns[row.well - 1].top_fill_idx !== idx + 1) continue;
+
+			// well is 4 row deep
+			if (row.well != this.rows[idx-1].well
+				|| row.well != this.rows[idx-2].well
+				|| row.well !=  this.rows[idx-3].well
+			) continue;
+
+			return this.rows[idx-3];
+		}
+
+		return null;
+	}
+
+	hasPerfectSlope() {
+		let height_idx = -1;
+
+		for (let col of this.columns) {
+			if (col.has_holes) return false;
+			if (col.top_fill_idx < height_idx) return false;
+
+			height_idx = col.top_fill_idx;
+		}
+
+		return true;
+	}
+
+	hasDoubleWell() {
+		if (
+			!this.is_tetris_ready
+			|| this.tetris_top_row.idx === 0 // tetris ready all the way to the top
+			|| this.tetris_top_row.well != 10
+		) return false;
+
+		for (let idx = this.tetris_top_row.idx; idx--; ) {
+			let prev_row = this.rows[idx];
+
+			if (prev_row.well === 10) continue; // tetris well still going!
+			if (prev_row.emptys.length == 2 && prev_row.emptys[0] === 8 && prev_row.emptys[1] === 9) {
+				return true;
+			}
+
+			return false;
+		}
+
+		return false;
+	}
+}
+
 class Game {
 	constructor(event) {
 		// will store all pieces that have been played in the game
