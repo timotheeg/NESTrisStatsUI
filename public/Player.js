e1bf111842b7fdb211266c8ffbf29dff428427f6
Timothee Groleau <tgroleau@zendesk.com>
2020-07-05 18:57:44 +0800

    Add renderer for Invisible Tetris

--- public/Player.js
+++ public/Player.js
@@ -1,7 +1,7 @@
 function renderBlock(level, block_index, pixel_size, ctx, pos_x, pos_y) {
 	let color;
 
-	if (block_index < 1 || block_index > 3) {
+	if (block_index < 1 || block_index > 6) {
 		return;
 	}
 
@@ -65,6 +65,44 @@ function renderBlock(level, block_index, pixel_size, ctx, pos_x, pos_y) {
 				pos_x + pixel_size,
 				pos_y + pixel_size * 2,
 				pixel_size,
+				pixel_size
+			);
+
+			break;
+
+		case 4:
+		case 5:
+		case 6:
+			if (block_index === 4) {
+				color = '#FFFFFF';
+			}
+			else {
+				color = LEVEL_COLORS[level % 10][block_index - 5];
+			}
+
+			ctx.fillStyle = `${color}90`;
+			ctx.fillRect(
+				pos_x,
+				pos_y,
+				pixel_size * 7,
+				pixel_size
+			);
+			ctx.fillRect(
+				pos_x,
+				pos_y + pixel_size,
+				pixel_size,
+				pixel_size * 6
+			);
+			ctx.fillRect(
+				pos_x + pixel_size * 6,
+				pos_y + pixel_size,
+				pixel_size,
+				pixel_size * 6
+			);
+			ctx.fillRect(
+				pos_x + pixel_size,
+				pos_y + pixel_size * 6	,
+				pixel_size * 5,
 				pixel_size
 			);
 
