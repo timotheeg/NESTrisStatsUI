43b8e8badfa2363f721125630e2c957bb6bbf58c
Timothee Groleau <tgroleau@zendesk.com>
2020-07-19 19:05:43 +0800

    Classic Tetris Bot should not be talking

--- voiceover.js
+++ voiceover.js
@@ -228,6 +228,8 @@ if (config.chat_voice && config.chat_voice.enabled) {
 	const say = eval(`say_${config.chat_voice.provider}`);
 
 	module.exports = function(chatter) {
+		if (chatter.username == "classictetrisbot") return;
+
 		const [introduced] = getVoice(chatter.username);
 
 		if (!introduced) {
