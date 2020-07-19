dd89027c8b664c4b3c115f7869fa9a4ed51d8b52
Timothee Groleau <tgroleau@zendesk.com>
2020-07-19 08:59:49 +0800

    Allows avatar to be found locally from player names

--- public/Player.js
+++ public/Player.js
@@ -306,11 +306,18 @@ class Player {
 	}
 
 	setAvatar(url) {
-		this.avatar.style.backgroundImage = `url('${url}')`;
+		if (!url) {
+			// try local files...
+			url = `./avatars/${this.player_name}.png`;
+		}
+
+		this.avatar_url = url;
+		this.avatar.style.backgroundImage = `url('${encodeURI(url)}')`;
 	}
 
 	setName(name) {
-		this.dom.name.textContent = name;
+		this.dom.name.textContent = this.player_name = name;
+		this.setAvatar();
 	}
 
 	setFrame(data) {
