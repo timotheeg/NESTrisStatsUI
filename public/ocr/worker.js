137a50440024e9fe436c1cda70e588959506711f
Timothee Groleau <timothee.groleau@gmail.com>
2020-08-13 10:22:27 +0800

    Lots of experimentations to gain speed + scale in JS

--- public/ocr/worker.js
+++ public/ocr/worker.js
@@ -1,6 +1,8 @@
-const exports = {};
-importScripts('./loader.js');
+// const exports = {};
+// importScripts('./loader.js');
 const loader = exports;
+
+// importScripts('./image_tools.js');
 
 let asmodule;
 
@@ -26,28 +28,22 @@ scale_canvas_ctx.imageSmoothingQuality = 'medium';
 let raw_canvas;
 let raw_canvas_ctx;
 
+function roundedLuma(r, g, b) {
+	return Math.round(r * 0.299 + g * 0.587 + b * 0.114);
+}
 
-// brute force processing
-// candidate for assembly optimization
-function getDigit(img_data, max_check_index) {
-	const num_pixels = img_data.width * img_data.height;
-	const pixel_data = img_data.data;
-
-	if (!max_check_index) {
-		max_check_index = templates.length;
-	}
-
+function getDigit(pixel_data, max_check_index) {
 	const sums = new Uint32Array(max_check_index).fill(0);
+	const size = pixel_data.length >>> 2;
 
-	for (let p_idx=num_pixels; p_idx--; ) {
-		const offset_idx = p_idx * 4;
+	for (let p_idx = size; p_idx--; ) {
+		const offset_idx = p_idx << 2;
 		const luma = roundedLuma(
 			pixel_data[offset_idx],
 			pixel_data[offset_idx + 1],
 			pixel_data[offset_idx + 2],
 		);
 
-		// inline this loop
 		for (let t_idx=max_check_index; t_idx--; ) {
 			const diff = luma - templates[t_idx][p_idx];
 			sums[t_idx] += diff * diff;
@@ -95,16 +91,12 @@ async function getTemplateData(digit) {
 	return lumas;
 }
 
-function roundedLuma(r, g, b) {
-	return Math.round(r * 0.299 + g * 0.587 + b * 0.114);
-}
-
 async function loadDigits() {
 	return await Promise.all(DIGITS.map(getTemplateData));
 }
 
 let ready = false;
-let config = null;
+// let config = null;
 let drop_frames = 0;
 
 self.onmessage = function(msg) {
@@ -124,24 +116,21 @@ self.onmessage = function(msg) {
 			}
 			/**/
 
-			processFrame(msg.data.frame);
+			processFrame2(msg.data.frame);
 		}
 	}
 }
 
 function setConfig(_config) {
-	config = _config;
+	// config = _config;
 
-	getCaptureBounds();
+	processConfig();
 
 	raw_canvas = new OffscreenCanvas(config.width, config.height);
 	raw_canvas_ctx = raw_canvas.getContext('2d', { alpha: false, lowLatency: true });
 }
 
-let capture_bounds;
-let capture_area;
-
-function getCaptureBounds() {
+function processConfig() {
 	const bounds = {
 		top:    0xFFFFFFFF,
 		left:   0xFFFFFFFF,
@@ -149,15 +138,27 @@ function getCaptureBounds() {
 		right:  -1,
 	}
 
-	for (const { crop: [x, y, w, h] } of Object.values(config.tasks)) {
+	for (const task of Object.values(config.tasks)) {
+		const { crop: [x, y, w, h] } = task;
+
 		bounds.top    = Math.min(bounds.top,    y);
 		bounds.left   = Math.min(bounds.left,   x);
 		bounds.bottom = Math.max(bounds.bottom, y + h);
 		bounds.right  = Math.max(bounds.right,  x + w);
+
+		task.crop_img = new ImageData(w, h);
+
+		if (task.pattern) {
+			task.scale_img = new ImageData(
+				(8 * task.pattern.length - 1) * 2,
+				14
+			);
+		}
 	}
 
-	capture_bounds = bounds;
-	capture_area   = {
+	config.digit_img = new ImageData(14, 14);
+	config.capture_bounds = bounds;
+	config.capture_area   = {
 		x: bounds.left,
 		y: bounds.top,
 		w: bounds.right - bounds.left,
@@ -165,7 +166,7 @@ function getCaptureBounds() {
 	};
 }
 
-function processFrame(frame) {
+function processFrame2(frame) {
 	performance.mark('start');
 
 		// load the raw frame
@@ -173,19 +174,19 @@ function processFrame(frame) {
 
 	performance.mark('draw_end');
 
-	deinterlace();
+	const source_img = deinterlace();
 
 	performance.mark('deinterlace_end');
 
-	const score = ocrDigits(config.tasks.score);
+	const score = ocrDigits(source_img, config.tasks.score);
 
 	performance.mark('score_end');
 
-	const level = 0; // ocrDigits(config.tasks.level);
+	const level = ocrDigits(source_img, config.tasks.level);
 
 	performance.mark('level_end');
 
-	const lines = 0; // ocrDigits(config.tasks.lines);
+	const lines = ocrDigits(source_img, config.tasks.lines);
 
 	performance.mark('lines_end');
 
@@ -216,7 +217,7 @@ function processFrame(frame) {
 	performance.clearMarks();
 	performance.clearMeasures();
 
-	self.postMessage(res);
+	onMessage(res);
 }
 
 // TODO: do with assembly
@@ -224,12 +225,15 @@ function processFrame(frame) {
 function deinterlace() {
 	performance.mark('di_start');
 
-	const pixels = raw_canvas_ctx.getImageData(capture_area.x, 0, capture_area.w, capture_bounds.bottom * 2);
+	const pixels = raw_canvas_ctx.getImageData(
+		config.capture_area.x, 0,
+		config.capture_area.w, config.capture_bounds.bottom * 2 // * 2 to account for interlacing
+	);
 
 	performance.mark('di_crop_end');
 
-	const pixels_per_rows = capture_area.w * 4;
-	const max_rows = capture_bounds.bottom + 1;
+	const pixels_per_rows = config.capture_area.w * 4;
+	const max_rows = config.capture_bounds.bottom;
 
 	for (let row_idx = 1; row_idx < max_rows; row_idx++) {
 		pixels.data.copyWithin(
@@ -241,29 +245,32 @@ function deinterlace() {
 
 	performance.mark('di_process_end');
 
-	raw_canvas_ctx.putImageData(pixels, capture_area.x, 0, 0, 0, pixels_per_rows, max_rows);
+	// raw_canvas_ctx.putImageData(pixels, capture_area.x, 0, 0, 0, pixels_per_rows, max_rows);
+	// processed_ctx.putImageData(pixels, config.capture_area.x, 0, 0, 0, pixels_per_rows, max_rows);
 
 	performance.mark('di_write_end');
+
+	return pixels;
 }
 
-function ocrDigits(task) {
-	performance.mark('ocr_start');
-
-	const [x, y, w, h] = task.crop;
+function ocrDigits(source_img, task) {
+	const [raw_x, y, w, h] = task.crop;
 	const nominal_width = 8 * task.pattern.length - 1;
 
-	// draw 2x scale with smoothing
-	scale_canvas_ctx.drawImage(
-		raw_canvas,
-		x, y, w, h,
-		0, 0, nominal_width * 2, 14
-	);
+	const x = raw_x - config.capture_area.x;
 
-	let crop = 0;
-	let alloc = 0;
-	let ocr = 0;
+	performance.mark('ocr_start');
+
+	crop(source_img, x, y, w, h, task.crop_img);
+	bicubic(task.crop_img, task.scale_img);
+
+	dom.score_scaled.getContext('2d').putImageData(task.scale_img, 0, 0)
 
 	performance.mark('ocr_scale_end');
+
+	let t_crop = 0;
+	let alloc = 0;
+	let ocr = 0;
 
 	const digits = new Array(task.pattern.length);
 
@@ -272,12 +279,16 @@ function ocrDigits(task) {
 	for (let idx=digits.length; idx--; ) {
 		then = performance.now();
 		const char = task.pattern[idx];
-		const image_data = scale_canvas_ctx.getImageData(idx * 16, 0, 14, 14);
+		// const image_data = scale_canvas_ctx.getImageData(idx * 16, 0, 14, 14);
+		crop(task.scale_img, idx * 16, 0, 14, 14, config.digit_img)
 
-		crop += performance.now() - then;
+		t_crop += performance.now() - then;
 		then = performance.now();
 
-		// const digit = getDigit(image_data, PATTERN_MAX_INDEXES[char]); // only check numerical digits and null
+		/**/
+		const digit = getDigit(config.digit_img.data, PATTERN_MAX_INDEXES[char]); // only check numerical digits and null
+		/**/
+		/*
 		const as_array = asmodule.__retain(asmodule.__allocArray(asmodule.Uint8ArrayId, image_data.data));
 		alloc += performance.now() - then;
 		then = performance.now();
@@ -287,9 +298,10 @@ function ocrDigits(task) {
 			PATTERN_MAX_INDEXES[char]
 		);
 
-		ocr += performance.now() - then;
-
 		asmodule.__release(as_array);
+		/**/
+
+		ocr += performance.now() - then;
 
 		// if (!digit) return null;
 
@@ -300,11 +312,11 @@ function ocrDigits(task) {
 
 	// TODO: compute the number, rather than returning an array of digits
 	// TODO: can add in the for loop above
-	return [...digits, {crop: crop.toFixed(3), alloc: alloc.toFixed(3), ocr: ocr.toFixed(3)}];
+	return [...digits, {crop: t_crop.toFixed(3), alloc: alloc.toFixed(3), ocr: ocr.toFixed(3)}];
 }
 
 
-async function init() {
+(async function init() {
 	try {
 		const importObject = {
 			env: {
@@ -322,7 +334,7 @@ async function init() {
 		asmodule = exports;
 
 		// load the digit templates for OCR
-		const luma_templates = await loadDigits();
+		const luma_templates = templates = await loadDigits();
 		const size = luma_templates[0].length;
 		const merged_templates = new Uint8Array(luma_templates.length * size);
 
@@ -339,6 +351,4 @@ async function init() {
 	catch(err) {
 		console.error(err);
 	}
-}
-
-init();
+})();
